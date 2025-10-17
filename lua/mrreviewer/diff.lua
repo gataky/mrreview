78946ab4237ce7135569d64cf9ca63a6de01d4aa
Jeff Oryen <jeffor@zillowgroup.com>
2025-10-17 08:57:11 -0700

    Fix comment list nav

--- lua/mrreviewer/diff.lua
+++ lua/mrreviewer/diff.lua
@@ -237,6 +237,79 @@ function M.create_unified_view(old_lines, new_lines, file_info)
   return buf
 end
 
+--- Set up keymaps for diff navigation
+local function setup_keymaps()
+  local config = require('mrreviewer.config')
+  local keymaps = config.get_value('keymaps')
+
+  if not keymaps then
+    return
+  end
+
+  -- Set keymaps for both buffers
+  for _, buf in pairs(M.state.buffers) do
+    if vim.api.nvim_buf_is_valid(buf) then
+      vim.api.nvim_buf_set_keymap(buf, 'n', keymaps.next_file or ']f', '', {
+        callback = M.next_file,
+        noremap = true,
+        silent = true,
+        desc = 'Next file in MR',
+      })
+
+      vim.api.nvim_buf_set_keymap(buf, 'n', keymaps.prev_file or '[f', '', {
+        callback = M.prev_file,
+        noremap = true,
+        silent = true,
+        desc = 'Previous file in MR',
+      })
+
+      vim.api.nvim_buf_set_keymap(buf, 'n', keymaps.close or 'q', '', {
+        callback = M.close,
+        noremap = true,
+        silent = true,
+        desc = 'Close diff view',
+      })
+
+      vim.api.nvim_buf_set_keymap(buf, 'n', keymaps.next_comment or ']c', '', {
+        callback = comments.next_comment,
+        noremap = true,
+        silent = true,
+        desc = 'Next comment',
+      })
+
+      vim.api.nvim_buf_set_keymap(buf, 'n', keymaps.prev_comment or '[c', '', {
+        callback = comments.prev_comment,
+        noremap = true,
+        silent = true,
+        desc = 'Previous comment',
+      })
+
+      vim.api.nvim_buf_set_keymap(buf, 'n', keymaps.toggle_comments or '<leader>tc', '', {
+        callback = comments.toggle_mode,
+        noremap = true,
+        silent = true,
+        desc = 'Toggle comment display mode',
+      })
+
+      vim.api.nvim_buf_set_keymap(buf, 'n', keymaps.show_comment or 'K', '', {
+        callback = comments.show_float_for_current_line,
+        noremap = true,
+        silent = true,
+        desc = 'Show comment for current line',
+      })
+
+      vim.api.nvim_buf_set_keymap(buf, 'n', keymaps.list_comments or '<leader>cl', '', {
+        callback = function()
+          require('mrreviewer.commands').list_comments()
+        end,
+        noremap = true,
+        silent = true,
+        desc = 'List all comments in MR',
+      })
+    end
+  end
+end
+
 --- Open diff view for a specific file in the MR
 --- @param mr_data table MR details with diff_refs
 --- @param file_info table File information
@@ -270,6 +343,9 @@ function M.open_file_diff(mr_data, file_info)
 
   -- Display comments for the buffer
   comments.display_for_file(file_info.new_path or file_info.path, buf)
+
+  -- Set up keymaps for the new buffer
+  setup_keymaps()
 
   utils.notify('Loaded diff for ' .. file_info.path, 'info')
 end
@@ -463,79 +539,6 @@ function M.load_file_in_existing_windows(mr_data, file_info)
   comments.display_for_file(file_info.new_path or file_info.path, buf)
 
   utils.notify('Loaded diff for ' .. file_info.path, 'info')
-end
-
---- Set up keymaps for diff navigation
-local function setup_keymaps()
-  local config = require('mrreviewer.config')
-  local keymaps = config.get_value('keymaps')
-
-  if not keymaps then
-    return
-  end
-
-  -- Set keymaps for both buffers
-  for _, buf in pairs(M.state.buffers) do
-    if vim.api.nvim_buf_is_valid(buf) then
-      vim.api.nvim_buf_set_keymap(buf, 'n', keymaps.next_file or ']f', '', {
-        callback = M.next_file,
-        noremap = true,
-        silent = true,
-        desc = 'Next file in MR',
-      })
-
-      vim.api.nvim_buf_set_keymap(buf, 'n', keymaps.prev_file or '[f', '', {
-        callback = M.prev_file,
-        noremap = true,
-        silent = true,
-        desc = 'Previous file in MR',
-      })
-
-      vim.api.nvim_buf_set_keymap(buf, 'n', keymaps.close or 'q', '', {
-        callback = M.close,
-        noremap = true,
-        silent = true,
-        desc = 'Close diff view',
-      })
-
-      vim.api.nvim_buf_set_keymap(buf, 'n', keymaps.next_comment or ']c', '', {
-        callback = comments.next_comment,
-        noremap = true,
-        silent = true,
-        desc = 'Next comment',
-      })
-
-      vim.api.nvim_buf_set_keymap(buf, 'n', keymaps.prev_comment or '[c', '', {
-        callback = comments.prev_comment,
-        noremap = true,
-        silent = true,
-        desc = 'Previous comment',
-      })
-
-      vim.api.nvim_buf_set_keymap(buf, 'n', keymaps.toggle_comments or '<leader>tc', '', {
-        callback = comments.toggle_mode,
-        noremap = true,
-        silent = true,
-        desc = 'Toggle comment display mode',
-      })
-
-      vim.api.nvim_buf_set_keymap(buf, 'n', keymaps.show_comment or 'K', '', {
-        callback = comments.show_float_for_current_line,
-        noremap = true,
-        silent = true,
-        desc = 'Show comment for current line',
-      })
-
-      vim.api.nvim_buf_set_keymap(buf, 'n', keymaps.list_comments or '<leader>cl', '', {
-        callback = function()
-          require('mrreviewer.commands').list_comments()
-        end,
-        noremap = true,
-        silent = true,
-        desc = 'List all comments in MR',
-      })
-    end
-  end
 end
 
 --- Open diff view for an MR
